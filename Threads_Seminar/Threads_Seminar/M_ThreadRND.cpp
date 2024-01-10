5a325636de771494c0a6fa8f70cb1be9ff860d27
Dass33 <94531241+Dass33@users.noreply.github.com>
2024-01-10 15:13:05 +0100

    Update M_ThreadRND.cpp

--- Threads_Seminar/Threads_Seminar/M_ThreadRND.cpp
+++ Threads_Seminar/Threads_Seminar/M_ThreadRND.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <Windows.h>
-#include <chrono>
+
 
 
 //lokalni promene jsou pro thready neviditelne 
@@ -9,14 +9,22 @@
 
 int pintRND_number;
 
+CRITICAL_SECTION pcsCriticalSection01;
+
 DWORD WINAPI RND_generator(LPVOID lpParm)
 {
 
 	while (true)
 	{
+		EnterCriticalSection(&pcsCriticalSection01);
+		//tread prestane mit pridelovany strojovi cas do te doby dokud neni kriticka sekce volna
+
+		//vzdycky ostetrit chyby
+		//osetrit opusteni funcke
 		pintRND_number++;
-		if (pintRND_number > 6) pintRND_number;
-		Sleep(200);
+		if (pintRND_number > 6) pintRND_number = 1;
+	
+		LeaveCriticalSection(&pcsCriticalSection01);
 	}
 
 	return 0;
@@ -27,9 +35,13 @@ DWORD WINAPI RND_reader(LPVOID lpParm)
 {
 	for (int i = 0; i < 20; i++)
 	{
+		EnterCriticalSection(&pcsCriticalSection01);
+
 		printf("%i, \n", pintRND_number);
+		LeaveCriticalSection(&pcsCriticalSection01);
+		
 		Sleep(200);
-	} 
+	}
 
 	return 0;
 }
@@ -41,8 +53,9 @@ int main()
 	HANDLE lhThread2d;
 
 	//init promene
-	pintRND_number = 0;
+	pintRND_number = 1;
 
+	InitializeCriticalSection(&pcsCriticalSection01);
 	//spusteni RND threadu
 	CreateThread(NULL,0,RND_generator, NULL,0, &ldwThread1Id);
 
